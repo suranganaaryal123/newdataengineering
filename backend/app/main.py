94dd6c7700806d0d7f89679eadb53582f6812ede
Surangana Aryal <145215217+suranganaaryal123@users.noreply.github.com>
2025-11-22 19:20:13 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
